2a74f738596e780b67e3a601c6fa17e37d38e918
Ioann Yaromenka <ioann.yaromenka@innowise.com>
2025-11-30 16:45:35 +0300

    cleanup

--- src/main/java/com/intern/orderservice/service/UserApiService.java
+++ src/main/java/com/intern/orderservice/service/UserApiService.java
@@ -1,7 +1,6 @@
 package com.intern.orderservice.service;
 
 import com.intern.orderservice.dto.response.UserResponse;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 public interface UserApiService {
 
