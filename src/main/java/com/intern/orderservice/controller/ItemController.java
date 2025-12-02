9cb888af51d8ef57b065a1c25dfd0dd3b5a5be5f
Ioann Yaromenka <ioann.yaromenka@innowise.com>
2025-12-02 17:52:22 +0300

    item controller endpoint name fix

--- src/main/java/com/intern/orderservice/controller/ItemController.java
+++ src/main/java/com/intern/orderservice/controller/ItemController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/items")
+@RequestMapping("/api/items")
 public class ItemController {
 
     private final ItemService itemService;
